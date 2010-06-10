f9ab35a1e93c3576c289a80c2ba9dc4a57db429a
Ask Solem <askh@opera.com>
2010-06-10 13:55:18 +0200

    Use new celery.events.state module instead of celerymonitor.state

--- celerymonitor/listener.py
+++ celerymonitor/listener.py
@@ -1,25 +1,17 @@
 from celery.events import EventReceiver
 from celery.messaging import establish_connection
 
-from celerymonitor.state import monitor_state
+from celery.events.state import state
 
 
 class EventListener(object):
     """Capture events sent by messages and store them in memory."""
 
-    def __init__(self, state=monitor_state):
+    def __init__(self, state=state):
         self.state = state
         self.connection = establish_connection()
-        self.receiver = EventReceiver(self.connection, handlers={
-            "task-received": state.receive_task_received,
-            "task-accepted": state.receive_task_event,
-            "task-succeeded": state.receive_task_event,
-            "task-retried": state.receive_task_event,
-            "task-failed": state.receive_task_event,
-            "worker-online": state.receive_worker_event,
-            "worker-offline": state.receive_worker_event,
-            "worker-heartbeat": state.receive_heartbeat,
-        })
+        self.receiver = EventReceiver(self.connection,
+                                      handlers={"*": self.state.event})
 
     def start(self):
         self.receiver.capture()
