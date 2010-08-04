81a5230ec1b321cc4f7bfc732667a5ab86c722a7
Ask Solem <askh@opera.com>
2010-08-04 16:38:22 +0200

    setup.py: Include data files at installation

--- setup.py
+++ setup.py
@@ -11,9 +11,59 @@ except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
     from setuptools import setup, find_packages, Command
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
 
 import celerymonitor as distmeta
 
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+src_dir = "celerymonitor"
+
+def osx_install_data(install_data):
+
+    def finalize_options(self):
+        self.set_undefined_options("install", ("install_lib", "install_dir"))
+        install_data.finalize_options(self)
+
+def fullsplit(path, result=None):
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+SKIP_EXTENSIONS = [".pyc", ".pyo", ".swp", ".swo"]
+
+
+def is_unwanted_file(filename):
+    for skip_ext in SKIP_EXTENSIONS:
+        if filename.endswith(skip_ext):
+            return True
+    return False
+
+for dirpath, dirnames, filenames in os.walk(src_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith("."):
+            del dirnames[i]
+    for filename in filenames:
+        if filename.endswith(".py"):
+            packages.append('.'.join(fullsplit(dirpath)))
+        elif is_unwanted_file(filename):
+            pass
+        else:
+            data_files.append([dirpath, [os.path.join(dirpath, f) for f in
+                filenames]])
 
 class RunTests(Command):
     description = "Run the test suite from the testproj dir."
@@ -60,7 +110,8 @@ setup(
     url=distmeta.__homepage__,
     platforms=["any"],
     license="BSD",
-    packages=find_packages(exclude=['ez_setup']),
+    packages=packages,
+    data_files=data_files,
     scripts=["bin/celerymon"],
     zip_safe=False,
     install_requires=install_requires,
